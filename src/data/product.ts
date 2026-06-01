a42b8c754ab3e80a90fb4ec33a0eedf5437e861d
Shabnam-Fatma <shabnamfatma.tech@gmail.com>
2026-06-02 04:25:31 +0530

    initial commit

---
+++ src/data/product.ts
@@ -0,0 +1,20 @@
+export const products = [
+    {
+        id: 1,
+        name: "Yamaha CLP765GP Digital Grand Piano",
+        price: 440990,
+        img: "https://cdn.shopaccino.com/johns-music/products/clp765gp-448455_l.jpg?v=706"
+    },
+    {
+        id: 2,
+        name: "Carlos Marshello CM39C Acoustic Guitar",
+        price: 12800,
+        img: "https://www.musicstores.in/img/p/1/1/0/5/7/11057.jpg"
+    },
+    {
+        id: 3,
+        name: "violenEstevam ESV -I 4/4 Violin",
+        price: 26500.00,
+        img: "https://thestockist.in/wp-content/uploads/2022/12/ESV-I-Violin1.jpg"
+    }
+]
